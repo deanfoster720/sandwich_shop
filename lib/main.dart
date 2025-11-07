c70aa894a25a1da043c2544fd06fe1e376251815
Dean Foster <deanfoster720@gmail.com>
2025-11-07 08:42:27 +0000

    Add test logic to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sandwich_shop/views/app_styles.dart';
 import 'package:sandwich_shop/repositories/order_repository.dart';
+import 'package:sandwich_shop/repositories/pricing_repository.dart';
 
 enum BreadType { white, wheat, wholemeal }
 
@@ -33,6 +34,7 @@ class OrderScreen extends StatefulWidget {
 
 class _OrderScreenState extends State<OrderScreen> {
   late final OrderRepository _orderRepository;
+  late final PricingRepository _pricingRepository;
   final TextEditingController _notesController = TextEditingController();
   bool _isFootlong = true;
   bool _isToasted = false; // new variable
@@ -42,6 +44,7 @@ class _OrderScreenState extends State<OrderScreen> {
   void initState() {
     super.initState();
     _orderRepository = OrderRepository(maxQuantity: widget.maxQuantity);
+    _pricingRepository = const PricingRepository();
     _notesController.addListener(() {
       setState(() {});
     });
@@ -102,6 +105,11 @@ class _OrderScreenState extends State<OrderScreen> {
     } else {
       noteForDisplay = _notesController.text;
     }
+
+    final double totalPrice = _pricingRepository.calculateTotalPrice(
+      quantity: _orderRepository.quantity,
+      isFootlong: _isFootlong,
+    );
 
     return Scaffold(
       appBar: AppBar(
