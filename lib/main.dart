d8341ccca7aa3fc16f5bd904e59cea65a87224fc
Dean Foster <deanfoster720@gmail.com>
2025-11-19 14:32:41 +0000

    Update sandwich preview to use BoxFit.contain instead of cover

--- lib/main.dart
+++ lib/main.dart
@@ -212,7 +212,7 @@ class _OrderScreenState extends State<OrderScreen> {
               height: 180,
               child: Image.asset(
                 _getCurrentImagePath(),
-                fit: BoxFit.cover,
+                fit: BoxFit.contain,
                 errorBuilder: (context, error, stackTrace) {
                   return const Center(
                     child: Text(
