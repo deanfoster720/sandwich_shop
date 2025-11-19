282df625e5a4ea891428d37d16eb5cf1cc806eb6
Dean Foster <deanfoster720@gmail.com>
2025-11-19 17:27:24 +0000

    Link cart to ProfileScreen via StyledButton and register /profile route for consistent navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sandwich_shop/views/about_screen.dart';
 import 'package:sandwich_shop/views/order_screen.dart';
+import 'package:sandwich_shop/views/profile_screen.dart';
 
 void main() {
   runApp(const App());
@@ -18,6 +19,8 @@ class App extends StatelessWidget {
         switch (settings.name) {
           case '/about':
             return MaterialPageRoute(builder: (_) => const AboutScreen());
+          case '/profile':
+            return MaterialPageRoute(builder: (_) => const ProfileScreen());
           default:
             return MaterialPageRoute(
                 builder: (_) => const OrderScreen(maxQuantity: 5));
