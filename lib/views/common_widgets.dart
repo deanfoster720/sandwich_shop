8d36283e25af27ea259df744272060456c82d5b9
Dean Foster <deanfoster720@gmail.com>
2025-11-28 00:02:11 +0000

    Refractor app bars into a reusable common widget

---
+++ lib/views/common_widgets.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sandwich_shop/models/cart.dart';
+import 'package:sandwich_shop/views/app_styles.dart';
+
+class ShopAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool showCartAction;
+
+  const ShopAppBar({super.key, required this.title, this.showCartAction = true});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          height: 100,
+          child: Image.asset('assets/images/logo.png'),
+        ),
+      ),
+      title: Text(
+        title,
+        style: heading1,
+      ),
+      actions: [
+        if (showCartAction)
+          Consumer<Cart>(
+            builder: (context, cart, child) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(Icons.shopping_cart),
+                    const SizedBox(width: 4),
+                    Text('${cart.countOfItems}'),
+                  ],
+                ),
+              );
+            },
+          ),
+      ],
+    );
+  }
+}
